1357a8b54ca2aceeb1813aac24c543bd6b72279a
Mourad De Clerck <mourad@risen.be>
2013-10-27 00:14:44 +0200

    include meetings and their friend in the trips json

--- app/serializers/meeting_serializer.rb
+++ app/serializers/meeting_serializer.rb
@@ -1,3 +1,4 @@
 class MeetingSerializer < ActiveModel::Serializer
   attributes :id, :trip_id, :friend_id, :location, :timeslots
+  has_one :friend
 end
--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,4 +1,5 @@
 class TripSerializer < ActiveModel::Serializer
   attributes :id, :name, :location, :from, :to
   has_many :friends
+  has_many :meetings
 end
