41a73069d55f42ca90fcaa0ca63f31937c86acf1
Mourad De Clerck <mourad@risen.be>
2013-10-26 23:52:04 +0200

    accept array of numbers for timeslots

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -39,6 +39,6 @@ class MeetingsController < ApplicationController
   end
 
   def meeting_params
-    params.permit(:trip_id, :friend_id, :location, :times)
+    params.permit(:trip_id, :friend_id, :location, times: [])
   end
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,6 @@
 class Meeting < ActiveRecord::Base
   belongs_to :trip
   belongs_to :friend
+
+  serialize :times, Array
 end
